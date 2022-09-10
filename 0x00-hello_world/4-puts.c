b0d855b8f9ba0546428be0b1ba821cec4d24a358
HBABDULFATAH <abdulfatahhb@gmail.com>
2022-09-09 18:53:45 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- * main - serves as the entry point for the program
+ *main - serves as the entry point for the program
  *
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ *return: by default returns zero if no erros otherwise returns non zero value
  */
 int main(void)
 {
