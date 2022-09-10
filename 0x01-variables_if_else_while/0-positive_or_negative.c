9d54a3ba6dcdcb710eec7ea935768c445a33ed94
HBABDULFATAH <abdulfatahhb@gmail.com>
2022-09-09 20:28:51 -0700

    V

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,6 @@
 
 #include <stdio.h>
 
-
-
 /**
  *
  * * main - print if the number is postive, zero, or negative
@@ -14,7 +12,7 @@
  *
  * * Description: using the main function
  *
- * * this program prints "Programming is positive, zero, or negative
+ * * this program prints "Programming is positive, zero, or negative"
  *
  * * Return: 0
  *
