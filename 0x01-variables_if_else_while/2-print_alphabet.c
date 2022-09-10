c4da14718a39442a4a50a64bf587e910edf08670
HBABDULFATAH <abdulfatahhb@gmail.com>
2022-09-09 21:50:17 -0700

    V

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  * main - print if the number is postive, zero, or negative
  *
  * Description: using the main function
@@ -8,13 +7,12 @@
  * Return: 0
  */
 int main(void)
-
 {
 	char ch;
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
-			putchar(ch);
+		putchar(ch);
 	}
-	putchar('\n');
-	return (0);
+		putchar('\n');
+		return (0);
 }
